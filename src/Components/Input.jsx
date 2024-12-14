e628e7cf7da505bb684fda8d360eae4cf5737002
shammasktl <mshammast370737@gmail.com>
2024-12-14 15:23:08 +0530

    Props Added for Data Collection from Inputs

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Input = ({ textarea, label, ...props }) => {
+const Input = ({ textarea, label, ref, ...props }) => {
   const classes = 'w-full p-1 border-b-2 rounded-sm border-stone-300  bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600'
   return (
     <p className='flex flex-col gap-2 my-4 '>
       <label htmlFor="" className='text-sm font-bold uppercase text-stone-500'>{label}</label>
       {textarea ? 
-      (<textarea placeholder={`Enter ${label}`} className={classes} {...props} />) :
-      (<input className={classes} placeholder={`Enter ${label}`} {...props} />)}
+      (<textarea ref={ref} placeholder={`Enter ${label}`} className={classes} {...props} />) :
+      (<input ref={ref} className={classes} placeholder={`Enter ${label}`} {...props} />)}
     </p>
   )
 }
--- src/Components/NewProject.jsx
+++ src/Components/NewProject.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Input from './Input'
 
 const NewProject = () => {
+  
+  const titleRef = useRef(null)
+  const descriptionRef = useRef(null)
+  const dueDateRef = useRef(null)
+
   return (
     <div className='w-[35rem] mt-16'>
       <menu className='flex items-center justify-end gap-4 my-4 '>
@@ -9,9 +14,9 @@ const NewProject = () => {
         <li><button className='capitalize bg-stone-800 text-stone-50 duration-300 rounded-md px-6 py-2 hover:bg-stone-950'>save</button></li>
       </menu>
       <div className="">
-        <Input label="Title"/>
-        <Input label="Description" textarea={true}/>
-        <Input label="Due Date"/>
+        <Input ref={titleRef} label="Title"/>
+        <Input ref={descriptionRef} label="Description" textarea={true}/>
+        <Input ref={dueDateRef} label="Due Date"/>
       </div>
     </div>
   )
